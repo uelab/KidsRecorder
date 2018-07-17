7f21d34283578661d44fea069dc3e93c6bb59c80
NETID\mingrui <mingrui@uw.edu>
2018-07-16 18:50:32 -0700

    Recorder Manager in progress

--- app/src/main/java/com/userempowermentlab/kidsrecorder/Recording/BasicRecorder.java
+++ app/src/main/java/com/userempowermentlab/kidsrecorder/Recording/BasicRecorder.java
@@ -1,8 +1,90 @@
 package com.userempowermentlab.kidsrecorder.Recording;
 
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.MediaRecorder;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+
+import omrecorder.AudioChunk;
+import omrecorder.AudioRecordConfig;
+import omrecorder.OmRecorder;
+import omrecorder.PullTransport;
+import omrecorder.PullableSource;
+import omrecorder.Recorder;
+
 /**
  * Created by mingrui on 7/16/2018.
  */
 
-public class BasicRecorder {
+public class BasicRecorder implements PullTransport.OnAudioChunkPulledListener{
+    //configuration variables
+    private String filePath = null;
+    private Recorder recorder = null;
+    private boolean isRecording;
+
+    public boolean isRecording() {
+        return isRecording;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public void Start(){
+        if (filePath == null) return;
+        isRecording = true;
+        if(recorder == null) {
+            recorder = OmRecorder.wav(
+                    new PullTransport.Default(mic(), BasicRecorder.this),
+                    new File(filePath));
+        }
+        recorder.startRecording();
+    }
+
+    public void Stop(){
+        isRecording = false;
+        if (recorder != null){
+            try {
+                recorder.stopRecording();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                recorder = null;
+            }
+        }
+    }
+
+    public void Pause(){
+        isRecording = false;
+        if (recorder != null){
+            recorder.pauseRecording();
+        }
+    }
+
+    public void Resume(){
+        isRecording = true;
+        if (recorder != null){
+            recorder.resumeRecording();
+        }
+    }
+
+    @Override
+    public void onAudioChunkPulled(AudioChunk audioChunk) {
+        //passs
+    }
+
+    private PullableSource mic() {
+        return new PullableSource.Default(
+                new AudioRecordConfig.Default(
+                        MediaRecorder.AudioSource.MIC, AudioFormat.ENCODING_PCM_16BIT,
+                        AudioFormat.CHANNEL_IN_MONO, 44100
+                )
+        );
+    }
 }
--- app/src/main/java/com/userempowermentlab/kidsrecorder/Recording/RecordingManager.java
+++ app/src/main/java/com/userempowermentlab/kidsrecorder/Recording/RecordingManager.java
@@ -1,8 +1,95 @@
 package com.userempowermentlab.kidsrecorder.Recording;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.PowerManager;
+
 /**
  * Created by mingrui on 7/16/2018.
  */
 
-public class RecordingManager {
+public class RecordingManager extends Service {
+    private int recordingTime = 0; // 0 for manually stop recording; > 0 for limited recording time in ms
+    private int bufferSize = 0; // buffer file or not
+    private boolean alwaysRunning = false; //always run in background (useful if want to record when the app is in background)
+
+    private BasicRecorder recorder = null;
+    PowerManager.WakeLock wakeLock;
+    private final IBinder mBinder = new LocalBinder();
+
+    public void setRecordingTime(int recordingTime) {
+        this.recordingTime = recordingTime;
+    }
+
+    public void setAlwaysRunning(boolean alwaysRunning) {
+        this.alwaysRunning = alwaysRunning;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        recorder = new BasicRecorder();
+    }
+
+    //returns the instance of the service
+    public class LocalBinder extends Binder {
+        public RecordingManager getServiceInstance(){
+            return RecordingManager.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (alwaysRunning){
+            PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+            wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                    "MyWakelockTag");
+            wakeLock.acquire();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (alwaysRunning && wakeLock != null) {
+            wakeLock.release();
+        }
+    }
+
+    public void StartRecording(String filename) {
+        recorder.setFilePath(filename);
+        recorder.Start();
+    }
+
+    public void StartRecording(String filename, int timeLimit){
+        recorder.setFilePath(filename);
+        recordingTime = timeLimit;
+        recorder.Start();
+    }
+
+    public void StopRecording() {
+        if (recorder.isRecording()){
+            recorder.Stop();
+        }
+    }
+
+    public void PauseRecording() {
+        if (recorder.isRecording()){
+            recorder.Pause();
+        }
+    }
+
+    public void ResumeRecording() {
+        if (recorder.isRecording()){
+            recorder.Resume();
+        }
+    }
 }
